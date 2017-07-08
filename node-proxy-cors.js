c344fbb94e3bfeb50b10c5211daf7329dbb6a585
Jonathan <jmartinez@cymsuroeste.com>
2017-07-08 20:55:15 +0200

    moving to github

---
+++ node-proxy-cors.js
@@ -0,0 +1,24 @@
+/*
+    A NodeJS proxy to make CORS requests.
+        1. node node-proxy-cors
+        2. Make a request to http://localhost:8080/?url=http://www.someapi.com
+*/
+var express = require('express');
+var request = require('request');
+
+var app = express();
+
+//enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//proxy
+app.use('/', function(req, res) {
+    var url = req.query.url;
+    req.pipe(request(url)).pipe(res);
+});
+
+app.listen(8080);
